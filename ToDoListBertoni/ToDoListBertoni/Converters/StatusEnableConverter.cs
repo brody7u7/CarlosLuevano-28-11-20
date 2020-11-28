021949f377854049375424ec6d5b42bd8d4236c9
brody7u7 <brodyn.n@hotmail.com>
2020-11-28 15:19:33 -0600

    Complete button is disabled if todo item is already completed

---
+++ ToDoListBertoni/ToDoListBertoni/Converters/StatusEnableConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ToDoListBertoni.Converters
+{
+    public class StatusEnableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Models.ToDoStatus)
+                return ((Models.ToDoStatus)value) == Models.ToDoStatus.Incomplete;
+            throw new ArgumentException("value is not ToDoStatus type");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
